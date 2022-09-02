d62b2d02839159c9d5d8b4868bb15259ebcbc3b6
ajay-developer <84758365+ajay-dontha@users.noreply.github.com>
2022-09-02 18:02:53 +0530

    Add files via upload

---
+++ JavaScript/04_Js_Alerts/js/script.js
@@ -0,0 +1,34 @@
+/**
+ * Three diff types of alerts:
+ * 
+ * Alert
+ * Prompt
+ * Confirm
+ */
+
+//Alert
+alert("Alert: Welcome to JavaScript"); //display message
+
+//Confirm
+confirm("Confirm: Do you want to continue");//Ok or cancel
+
+//Prompt
+prompt("Enter Values:")
+
+var value=prompt("Enter Value:");
+console.log(value);
+
+//adding two numbers using prompt
+var value1=Number(prompt("Enter value1"));
+var value2=Number(prompt("Enter Value2"));
+var sum=(value1+value2);
+console.log(sum);
+
+//Multiplication
+var mul1=Number(prompt("Enter value1"));
+var mul2=Number(prompt("Enter Value2"));
+var mul=(mul1*mul2);
+console.log(mul);
+
+//Division
+
---
+++ JavaScript/05_Function/Js/script.js
@@ -0,0 +1,28 @@
+//functions without Parameters:
+
+function displayNames() {
+    console.log("Hello World");
+}
+displayNames();//function call
+
+//with parameters
+
+function June(name) {
+    console.log(name);
+}
+June("Allari Pillodu");
+
+//two parameters passing with functions
+
+function fullName(name1,name2){
+    console.log(name1,name2);
+}
+fullName("Shiva","Shankar")
+
+//three parameter
+
+function students(R1,R2,R3){
+    console.log(R1,R2,R3);
+    console.log(R2);
+}
+students("Sahithi","Aditya","AnuPriya");
